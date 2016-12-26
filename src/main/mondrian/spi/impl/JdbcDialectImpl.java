e3980baaac5f852ed9fd8769ae6f6b833de057c0
xiuyanduan <xiuyanduan@gmail.com>
2016-12-26 14:53:43 +0800

    Update JdbcDialectImpl.java

--- src/main/mondrian/spi/impl/JdbcDialectImpl.java
+++ src/main/mondrian/spi/impl/JdbcDialectImpl.java
@@ -1076,6 +1076,8 @@ public class JdbcDialectImpl implements Dialect {
             return DatabaseProduct.INGRES;
         } else if (productName.equals("Interbase")) {
             return DatabaseProduct.INTERBASE;
+        } else if (upperProductName.equals("KYLIN")) {
+            return DatabaseProduct.KYLIN;
         } else if (upperProductName.equals("LUCIDDB")
             || upperProductName.equals("OPTIQ"))
         {
